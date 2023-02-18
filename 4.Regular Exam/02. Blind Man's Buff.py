6be33db5d9ed7f6e78b9d3ef3a6b190ddeaadc25
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-02-18 15:27:29 +0200

    Initial commit

--- 4.Regular Exam/02. Blind Man's Buff.py
+++ 4.Regular Exam/02. Blind Man's Buff.py
@@ -3,7 +3,6 @@ playground = []
 for i in range(n):
     playground.append(input().split())
 
-# Find the initial position of the blindfolded player
 x, y = None, None
 for i in range(n):
     for j in range(m):
@@ -20,7 +19,6 @@ while True:
     if command == 'Finish':
         break
 
-    # Calculate the new position based on the command
     new_x, new_y = x, y
     if command == 'up':
         new_x -= 1
@@ -31,27 +29,21 @@ while True:
     elif command == 'right':
         new_y += 1
 
-    # Check if the new position is inside the playground
     if not (0 <= new_x < n and 0 <= new_y < m):
         continue
 
-    # Check if the new position is an obstacle
     if playground[new_x][new_y] == 'O':
         continue
 
-    # Check if the new position is an opponent
     if playground[new_x][new_y] == 'P':
         touched += 1
         playground[new_x][new_y] = '-'
 
-    # Update the position of the blindfolded player
     x, y = new_x, new_y
     moves += 1
 
-    # Check if all opponents have been touched
     if touched == 3:
         break
 
-# Print the game report
 print("Game over!")
 print(f"Touched opponents: {touched} Moves made: {moves}")
